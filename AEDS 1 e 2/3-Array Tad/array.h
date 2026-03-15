500312b5c318e8c26e53ddd4b740757a43d9b297
Nicolas Dias <nicolasbhgamer@gmail.com>
2026-03-15 18:09:43 -0300

    feat: add

--- AEDS 1 e 2/3-Array Tad/array.h
+++ AEDS 1 e 2/3-Array Tad/array.h
@@ -9,4 +9,7 @@ typedef struct Array {
 array_t new_array(int size);
 void print_array(array_t *arr);
 void append(array_t *arr, int n);
+void insert_at(array_t *arr, int n, int index);
+void remove_at(array_t *arr, int index);
+void delete_array(array_t *arr);
 void resize(array_t *arr);
--- AEDS 1 e 2/3-Array Tad/main.c
+++ AEDS 1 e 2/3-Array Tad/main.c
@@ -8,13 +8,18 @@ int main()
 
     for (int i = 0; i < 5; i++)
     {
-        arr.A[i] = i;
-        arr.length++;
+        append(&arr, i);
     }
 
     print_array(&arr);
     append(&arr, 100);
+    insert_at(&arr, 5000, 6);
+    print_array(&arr);
+    remove_at(&arr, 4);
+    remove_at(&arr, 9);
     print_array(&arr);
 
+
+    delete_array(&arr);
     return 0;
 }
