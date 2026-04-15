254f747635bd1ff087617c90c626d2edb302e40b
Nicolas Dias <nicolasbhgamer@gmail.com>
2026-04-15 12:23:34 -0300

    refactor: fix code identation

--- AEDS 1 e 2/TADS-Flexiveis/List/list.c
+++ AEDS 1 e 2/TADS-Flexiveis/List/list.c
@@ -3,28 +3,27 @@
 
 typedef struct Node
 {
-  	int val;
-    	struct Node *next;
+	int val;
+	struct Node *next;
 } node_t;
-
 
 typedef struct List
 {
-    	node_t *first;
-    	node_t *last;
-    	int counter;
+	node_t *first;
+	node_t *last;
+	int counter;
 } list_t;
 
 list_t create_list()
 {
-	list_t list = { .first = NULL, .last = NULL, .counter = 0 };
+	list_t list = {.first = NULL, .last = NULL, .counter = 0};
 	return list;
 }
 
 node_t *create_node(int val)
 {
 	node_t *node = malloc(sizeof(node_t));
-	
+
 	node->val = val;
 	node->next = NULL;
 
@@ -34,7 +33,7 @@ node_t *create_node(int val)
 void insert(list_t *list, int val)
 {
 	node_t *node = create_node(val);
-	if(list->counter == 0 && list->first == NULL)
+	if (list->counter == 0 && list->first == NULL)
 	{
 		list->first = node;
 		list->last = node;
@@ -50,23 +49,23 @@ void insert(list_t *list, int val)
 void free_list(list_t *list)
 {
 	node_t *curr = list->first;
-    	node_t *next;
-    
-   	while(curr != NULL)
-    	{	
-        	next = curr->next;
-        	free(curr);       
-        	curr = next;      
-    	}
-    	list->first = NULL;
-    	list->last = NULL;
-    	list->counter = 0;
+	node_t *next;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		free(curr);
+		curr = next;
+	}
+	list->first = NULL;
+	list->last = NULL;
+	list->counter = 0;
 }
 
 void print_list(list_t *list)
 {
 	node_t *curr = list->first;
-	while(curr != NULL)
+	while (curr != NULL)
 	{
 		printf("%d ", curr->val);
 		curr = curr->next;
@@ -76,7 +75,7 @@ void print_list(list_t *list)
 
 int main(void)
 {
-	int a=0 , b=1, c=2, d=3;
+	int a = 0, b = 1, c = 2, d = 3;
 	list_t list = create_list();
 	insert(&list, a);
 	insert(&list, b);
